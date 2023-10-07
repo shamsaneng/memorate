d639a4ba08be19a0f44ce01c764777b46e625b22
shamsaneng <57861890+shamsaneng@users.noreply.github.com>
2023-10-08 05:23:01 +0800

    Update index.py

--- index.py
+++ index.py
@@ -104,8 +104,8 @@ example_state = {
   "questions_rendered": True
 }
 
-openai.organization = keys.OPENAI_ORG
-openai.api_key = keys.OPENAI_KEY
+# openai.organization = keys.OPENAI_ORG
+openai.api_key = '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'
 
 ### Constants
 PROJECT_NAME = 'Memorate' # alt.: Memembered etc
